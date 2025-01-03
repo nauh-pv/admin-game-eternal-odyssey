2087e3c232577bde9bb61945d25f6eddedd58b04
nauh_pv <vanhuana4k56@gmail.com>
2025-01-04 01:30:44 +0700

    fix api wolrd and url cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://admin-game-eternalodyssey.vercel.app"],
+    allow_origins=["https://admin-game-eternalodyssey.vercel.app, https://admin-game-eternalodyssey.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/routes/world_routes.py
+++ backend/routes/world_routes.py
@@ -8,6 +8,7 @@ router = APIRouter(
 )
     
 @router.get("/", summary="Lấy danh sách tất cả các world")
+@router.get("", summary="Lấy danh sách tất cả các world")
 async def get_all_worlds():
     try:
         all_worlds = world_service.fetch_all_worlds()
@@ -16,6 +17,7 @@ async def get_all_worlds():
         raise HTTPException(status_code=500, detail=str(e))
     
 @router.get("/{world_id}", summary="Lấy thông tin chi tiết của World", description="API này trả về thông tin chi tiết của một World dựa trên world_id.")
+@router.get("/{world_id}/", summary="Lấy thông tin chi tiết của World", description="API này trả về thông tin chi tiết của một World dựa trên world_id.")
 async def get_world_detail(world_id: str):
     try:
         world_detail = world_service.fetch_world_detail_by_id(world_id)
@@ -29,6 +31,7 @@ async def get_world_detail(world_id: str):
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 @router.delete("/{world_id}", summary="Xóa một World", description="API này xóa một World dựa trên world_id.")
+@router.delete("/{world_id}/", summary="Xóa một World", description="API này xóa một World dựa trên world_id.")
 async def delete_world(world_id: str):
     try:
         result = world_service.delete_world_by_id(world_id)
@@ -41,6 +44,7 @@ async def delete_world(world_id: str):
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 @router.delete("/{world_id}/users/{user_id}", summary="Xóa người chơi khỏi World")
+@router.delete("/{world_id}/users/{user_id}/", summary="Xóa người chơi khỏi World")
 async def delete_user_world(world_id: str, user_id: str):
     try:
         result = world_service.delete_user_from_world(world_id, user_id)
@@ -53,6 +57,7 @@ async def delete_user_world(world_id: str, user_id: str):
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 @router.delete("/{world_id}/users/{user_id}/inventory/{item_id}", summary="Xóa vật phẩm khỏi Inventory")
+@router.delete("/{world_id}/users/{user_id}/inventory/{item_id}/", summary="Xóa vật phẩm khỏi Inventory")
 async def delete_item_inventory_user(world_id: str, user_id: str, item_id: str):
     try:
         result = world_service.delete_item_from_inventory(world_id, user_id, item_id)
@@ -65,6 +70,7 @@ async def delete_item_inventory_user(world_id: str, user_id: str, item_id: str):
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 @router.patch("/{world_id}", summary="Cập nhật thông tin World")
+@router.patch("/{world_id}/", summary="Cập nhật thông tin World")
 async def update_world(world_id: str, updated_data: dict):
     try:
         result = world_service.update_world_by_id(world_id, updated_data)
